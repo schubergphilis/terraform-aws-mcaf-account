cc5b52701b3a1d32bb7363e77774211998bb8750
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-10-07 12:18:39 +0200

    Remove prevent_destroy/lifecycle block
    Both resources support deletion so no longer applicable to have the prevent_destroy option set.
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 resource "mcaf_o365_alias" "default" {
   alias = var.email
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "mcaf_aws_account" "default" {
@@ -16,9 +12,5 @@ resource "mcaf_aws_account" "default" {
     firstname = var.sso_firstname
     lastname  = var.sso_lastname
     email     = var.sso_email
-  }
-
-  lifecycle {
-    prevent_destroy = true
   }
 }
